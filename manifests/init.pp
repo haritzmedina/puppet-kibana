336b5f8bdce79882136d71bf0592fa8d5db80e6e
Haritz Medina <hmedina@ikerlan.es>
2014-11-10 11:04:53 +0100

    Parametrized download and install locations. Added service consistency and works properly

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,28 +1,37 @@
 class kibana (
-  $downloadUrl = "https://download.elasticsearch.org/kibana/kibana/kibana-3.1.1.tar.gz",
+  $download_url = "https://download.elasticsearch.org/kibana/kibana/kibana-3.1.1.tar.gz",
+  $install_directory = "/usr/share/kibana",
 ){
 
   Exec {
     path => '/bin:/sbin:/usr/bin:/usr/sbin',
   }
 
-# Get kibana
+  # Get file name
+  $filenameArray = split($download_url, '/')
+  $basefilename = $filenameArray[-1]
+  $basename = regsubst($basefilename, '^(.+)\.(zip|tgz|tar\.\w+)$', '\1')
+
+  # Create install directory
+  file { $install_directory:
+    ensure => "directory"
+  }->
+  # Get kibana
   exec { "kibana_download":
-    command => "curl -O -L $downloadUrl",
+    command => "curl -o $install_directory/kibana.tar.gz -L $download_url",
   }->
-# Untar kibana and copy to instalation path
+  # Untar kibana and copy to instalation path
   exec { "kibana_extract":
-    command => "tar xf kibana-$version.tar.gz",
+    command => "tar xf $install_directory/kibana.tar.gz -C $install_directory",
   }->
-  exec { "kibana_move":
-    command => "cp -R kibana-$version /usr/share/kibana",
-  }->
-# Add alias to kibana on apache
+  #Remove temp files
+  file {"remove_temp_files":
+    ensure => absent,
+    path => "$install_directory/kibana.tar.gz"
+  }
+  # Add alias to kibana on apache and restart on config change
   exec { "kibana_apache_signup":
-    command => "echo 'alias /kibana /usr/share/kibana' > /etc/apache2/sites-enabled/kibana.conf"
-  }->
-# Restart apache service
-  exec { "kibana_apache_reload":
-      command => "service apache2 reload"
+    command => "echo 'alias /kibana /usr/share/kibana' > /etc/apache2/sites-enabled/kibana.conf",
+    notify => Service["apache2"]
   }
 }
